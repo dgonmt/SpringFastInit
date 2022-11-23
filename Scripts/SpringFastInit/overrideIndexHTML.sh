dd8d1ce360ef275b38442d8d43caccbfe4fb7dd7
this <dmitriy.goncharov@iths.se>
2022-11-23 01:34:00 +0100

    Added sh-files

---
+++ Scripts/SpringFastInit/overrideIndexHTML.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/bash
+echo "<!DOCTYPE html>" > ./app/src/main/resources/static/index.html
+echo "<html lang=\"en\">" >> ./app/src/main/resources/static/index.html
+echo "<head>" >> ./app/src/main/resources/static/index.html
+echo "    <meta charset=\"UTF-8\">" >> ./app/src/main/resources/static/index.html
+echo "    <title>Title</title>" >> ./app/src/main/resources/static/index.html
+echo "</head>" >> ./app/src/main/resources/static/index.html
+echo "<body>" >> ./app/src/main/resources/static/index.html
+echo "    <h1>Hello Shell Script :D</h1>" >> ./app/src/main/resources/static/index.html
+echo "</body>" >> ./app/src/main/resources/static/index.html
+echo "</html>" >> ./app/src/main/resources/static/index.html
+
