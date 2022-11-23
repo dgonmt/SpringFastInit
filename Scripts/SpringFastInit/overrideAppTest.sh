fb51b2cec0769bc240adb22bec1c4ed1642ec3cb
dgonmt <107300665+dgonmt@users.noreply.github.com>
2022-11-23 09:08:20 +0100

    Delete Scripts/SpringFastInit directory

--- Scripts/SpringFastInit/overrideAppTest.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "package se.iths;" > ./app/src/test/java/se/iths/AppTest.java
-echo "import org.junit.jupiter.api.Test;" >> ./app/src/test/java/se/iths/AppTest.java
-echo "import static org.junit.jupiter.api.Assertions.*;" >> ./app/src/test/java/se/iths/AppTest.java
-echo "class AppTest {" >> ./app/src/test/java/se/iths/AppTest.java
-echo "    @Test void appHasAGreeting() {" >> ./app/src/test/java/se/iths/AppTest.java
-echo "    }" >> ./app/src/test/java/se/iths/AppTest.java
-echo "}" >> ./app/src/test/java/se/iths/AppTest.java
